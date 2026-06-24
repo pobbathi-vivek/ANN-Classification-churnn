25d2598e6e645e83ddb96971e6fcdb520dcc96a8
pobbathi-vivek <107977681+pobbathi-vivek@users.noreply.github.com>
2026-06-24 10:47:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ prediction_proba = prediction[0][0]
 st.write(f'Churn Probability: {prediction_proba:.2f}')
 
 if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
+    st.write('The customer is likely to churn.')      ## This is a new feature
 else:
     st.write('The customer is not likely to churn.')
